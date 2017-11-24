929330e43af2775e850879c993d0b26460953bb7
Oussama Bonnor <oussamabonnor@gmail.com>
2017-11-24 17:30:12 +0100

    Add files via upload

---
+++ ENPO/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        Tarte tarteAuFraise = new Tarte();
+
+        //écrivez un contructeur dans la classe Tarte
+        //affectez des paramter d'entré au constructeur de la classe tarte
+
+        tarteAuFraise.declarerNom();
+        tarteAuFraise.declarerPrix();
+        tarteAuFraise.declarerGout();
+
+        //Creer une nouvel classe Cookie qui a comme attribues: prix (float) , diametre (int)
+        //ne creez aucun constructeur pour Cookie, affecter ces valeurs ici desus
+
+        Cookie cookieAuChocolat = new Cookie(); //cette ligne est une erreure car il n'y a pas de classe Cookie, créez la!
+
+        //exemple d'affectation:
+        // cookieAuChocolat.attribue = valeur attribué;
+    }
+}
---
+++ ENPO/src/Tarte.java
@@ -0,0 +1,20 @@
+public class Tarte {
+    String nom;
+    float prix;
+    String gout;
+
+    //Déclarer un constructeur ici
+
+    public void declarerPrix(){
+        System.out.println("cette tarte a le prix de: "+prix+" DA");
+    }
+
+    public void declarerNom(){
+        System.out.println("cette tarte est nommé: "+ nom);
+    }
+
+    public void declarerGout(){
+        System.out.println("cette tarte a un gout de: "+gout);
+    }
+}
+
